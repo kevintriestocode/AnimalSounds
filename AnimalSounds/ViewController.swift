5a1bca5f5bfc0ff3bc825fff001562c0f020db82
Kevin Wang <kevinwang@kevins-imac.local>
2018-02-19 09:58:41 -0500

    Now runs all 3 sounds.

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -24,13 +24,21 @@ class ViewController: UIViewController {
     
     @IBAction func catButtonTapped(_ sender: Any) {
         animalSoundLabel.text = "Meow!"
+        meowSound.play()
     }
     @IBAction func dogButtonTapped(_ sender: Any) {
         animalSoundLabel.text = "Woof!"
+        woofSound.play()
     }
     @IBAction func cowButtonTapped(_ sender: Any) {
         animalSoundLabel.text = "Moo!"
+        mooSound.play()
     }
+    
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
+    
     
     
 }
